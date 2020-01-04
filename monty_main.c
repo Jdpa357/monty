fa8b8a79caf8ea6b626f3b8bc5b79386a497a9fd
Jdpa357 <jdpa352@hotmail.es>
2020-01-04 16:58:53 +0000

    Adding main function

---
+++ monty_main.c
@@ -0,0 +1,19 @@
+#include "monty.h"
+
+/**
+ * main - Main function for the monty interpreter.
+ *
+ * Description: Monty bytecodes files interpreter, function in charge of
+ * executing other functions to carry on with execution processes
+ * @argc: Argument counter
+ - @argv: Argument vector
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char **argv)
+{
+	if (argc != 2)
+		nofile_error();
+	open_read(argv);
+	return (0);
+}
